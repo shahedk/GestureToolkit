3b055dc68c6a489e7ab2cc8a74778f4621183cc7
teddyseyed <teddyseyed@localhost>
2010-05-10 16:39:33 -0600

    Fixed some bugs in DistanceBetweenPoints.cs and added several tests to the according test class

--- Src/Net Framework/Gestures.Tests/Rules/Objects/DistanceBetweenPointsTest.cs
+++ Src/Net Framework/Gestures.Tests/Rules/Objects/DistanceBetweenPointsTest.cs
@@ -61,6 +61,9 @@ namespace Gestures.Tests.Rules.Objects
         }
 
         #endregion
+
+        #region Equals Tests
+
         [TestMethod()]
         public void EqualsTest_Null_Check()
         {
@@ -151,6 +154,302 @@ namespace Gestures.Tests.Rules.Objects
             Assert.AreEqual(expected, actual);
         }
 
+        #endregion
+
+        #region Union Tests
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "Invalid Union Type")]
+        public void Invalid_Union_Test_with_Increasing_and_Decreasing()
+        {
+
+            // The type we are testing
+            DistanceBetweenPoints target = new DistanceBetweenPoints()
+            {
+                Behaviour = BehaviourTypes.Increasing,
+                Max = -1,
+                Min = -1
+            };
+
+            // Another instance of same type of ruleData
+            IRuleData anotherRuleData = new DistanceBetweenPoints()
+            {
+                Behaviour = BehaviourTypes.Decreasing,
+                Max = -1,
+                Min = -1
+            };
+
+            // Since the behavior is different (increasing and decreasing), appropriate exception should be shown
+            target.Union(anotherRuleData);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "Invalid Union Type")]
+        public void Invalid_Union_Test_with_Decreasing_and_Increasing()
+        {
+
+            // The type we are testing
+            DistanceBetweenPoints target = new DistanceBetweenPoints()
+            {
+                Behaviour = BehaviourTypes.Decreasing,
+                Max = -1,
+                Min = -1
+            };
+
+            // Another instance of same type of ruleData
+            IRuleData anotherRuleData = new DistanceBetweenPoints()
+            {
+                Behaviour = BehaviourTypes.Increasing,
+                Max = -1,
+                Min = -1
+            };
+
+            // Since the behavior is different (decreasing and increasing), appropriate exception should be shown
+            target.Union(anotherRuleData);
+        }
+        #endregion
+
+        [TestMethod()]
+        public void Union_Unchanged_and_Increasing()
+        {
+
+            // The type we are testing
+            DistanceBetweenPoints target = new DistanceBetweenPoints()
+            {
+                Behaviour = BehaviourTypes.UnChanged,
+                Max = 2,
+                Min = 1
+            };
+
+            // Another instance of same type of ruleData
+            IRuleData anotherRuleData = new DistanceBetweenPoints()
+            {
+                Behaviour = BehaviourTypes.Increasing,
+                Max = 3,
+                Min = 2
+            };
+
+            //Union the 2 rules
+            target.Union(anotherRuleData);
+
+            //We expect the max to be the max of 2nd rule, which is 3
+            bool expected = true;
+            bool actual = target.Max.Equals(3);
+            Assert.AreEqual(expected, actual);
+
+            //We expect the behavior of the union'd rule to be "increasing", given the union types
+            expected = true;
+            actual = target.Behaviour.Equals("increasing");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Union_Unchanged_and_Decreasing()
+        {
+
+            // The type we are testing
+            DistanceBetweenPoints target = new DistanceBetweenPoints()
+            {
+                Behaviour = BehaviourTypes.UnChanged,
+                Max = 3,
+                Min = 1
+            };
+
+            // Another instance of same type of ruleData
+            IRuleData anotherRuleData = new DistanceBetweenPoints()
+            {
+                Behaviour = BehaviourTypes.Decreasing,
+                Max = 4,
+                Min = 0
+            };
+
+            //Union the 2 rules
+            target.Union(anotherRuleData);
+
+            //We expect the min to be the min of 2nd rule, which is 0
+            bool expected = true;
+            bool actual = target.Min.Equals(0);
+            Assert.AreEqual(expected, actual);
+
+            //We expect the behavior of the union'd rule to be "decreasing", given the union types
+            expected = true;
+            actual = target.Behaviour.Equals("decreasing");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Union_Unchanged_and_Unchanged()
+        {
+
+            // The type we are testing
+            DistanceBetweenPoints target = new DistanceBetweenPoints()
+            {
+                Behaviour = BehaviourTypes.UnChanged,
+                Max = 3,
+                Min = 1
+            };
+
+            // Another instance of same type of ruleData
+            IRuleData anotherRuleData = new DistanceBetweenPoints()
+            {
+                Behaviour = BehaviourTypes.UnChanged,
+                Max = 4,
+                Min = 0
+            };
+
+            //Union the 2 rules
+            target.Union(anotherRuleData);
+
+            //We expect the min to be the original min and different from the 2nd rule
+            bool expected = true;
+            bool actual = target.Min.Equals(0);
+            Assert.AreNotEqual(expected, actual);
+
+            //We expect the behavior of the union'd rule to be "unchanged", as nothing was union'd
+            expected = true;
+            actual = target.Behaviour.Equals("unchanged");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Union_Increasing_and_Unchanged()
+        {
+
+            // The type we are testing
+            DistanceBetweenPoints target = new DistanceBetweenPoints()
+            {
+                Behaviour = BehaviourTypes.Increasing,
+                Max = 3,
+                Min = 1
+            };
+
+            // Another instance of same type of ruleData
+            IRuleData anotherRuleData = new DistanceBetweenPoints()
+            {
+                Behaviour = BehaviourTypes.UnChanged,
+                Max = 4,
+                Min = 0
+            };
+
+            //Union the 2 rules
+            target.Union(anotherRuleData);
+
+            //We expect the max to be the original max (which is 3) and different from the 2nd rule
+            bool expected = true;
+            bool actual = target.Max.Equals(3);
+            Assert.AreEqual(expected, actual);
+
+            //We expect the behavior of the union'd rule to be "increasing", given the union types
+            expected = true;
+            actual = target.Behaviour.Equals("increasing");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Union_Decreasing_and_Unchanged()
+        {
+
+            // The type we are testing
+            DistanceBetweenPoints target = new DistanceBetweenPoints()
+            {
+                Behaviour = BehaviourTypes.Decreasing,
+                Max = 3,
+                Min = 1
+            };
+
+            // Another instance of same type of ruleData
+            IRuleData anotherRuleData = new DistanceBetweenPoints()
+            {
+                Behaviour = BehaviourTypes.UnChanged,
+                Max = 4,
+                Min = 0
+            };
+
+            //Union the 2 rules
+            target.Union(anotherRuleData);
+
+            //We expect the min to be the original min and different from the 2nd rule
+            bool expected = true;
+            bool actual = target.Min.Equals(0);
+            Assert.AreNotEqual(expected, actual);
+
+            //We expect the behavior of the union'd rule to still be "decreasing"
+            expected = true;
+            actual = target.Behaviour.Equals("decreasing");
+            Assert.AreEqual(expected, actual);
+        }
+
+        
+        [TestMethod()]
+        public void Union_Increasing_and_Increasing()
+        {
+
+            // The type we are testing
+            DistanceBetweenPoints target = new DistanceBetweenPoints()
+            {
+                Behaviour = BehaviourTypes.Increasing,
+                Max = 3,
+                Min = 1
+            };
+
+            // Another instance of same type of ruleData
+            IRuleData anotherRuleData = new DistanceBetweenPoints()
+            {
+                Behaviour = BehaviourTypes.Increasing,
+                Max = 4,
+                Min = 0
+            };
+
+            //Union the 2 rules
+            target.Union(anotherRuleData);
+
+            //We expect the max to be the max of 2nd rule, which is 4
+            bool expected = true;
+            bool actual = target.Max.Equals(4);
+            Assert.AreEqual(expected, actual);
+
+            //We expect the behavior of the union'd rule to be "increasing", given the union types
+            expected = true;
+            actual = target.Behaviour.Equals("increasing");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Union_Decreasing_and_Decreasing()
+        {
+
+            // The type we are testing
+            DistanceBetweenPoints target = new DistanceBetweenPoints()
+            {
+                Behaviour = BehaviourTypes.Decreasing,
+                Max = 3,
+                Min = 1
+            };
+
+            // Another instance of same type of ruleData
+            IRuleData anotherRuleData = new DistanceBetweenPoints()
+            {
+                Behaviour = BehaviourTypes.Decreasing,
+                Max = 4,
+                Min = 0
+            };
+
+            //Union the 2 rules
+            target.Union(anotherRuleData);
+
+            //We expect the min to be the min of 2nd rule, which is 0
+            bool expected = true;
+            bool actual = target.Min.Equals(0);
+            Assert.AreEqual(expected, actual);
+
+            //We expect the behavior of the union'd rule to be "decreasing", given the union types
+            expected = true;
+            actual = target.Behaviour.Equals("decreasing");
+            Assert.AreEqual(expected, actual);
+        }
+
+        #region toGDL Tests
+
         [TestMethod()]
         public void toGDL_Unchanged_test()
         {
@@ -163,11 +462,70 @@ namespace Gestures.Tests.Rules.Objects
             };
 
 
-            // Since the ruleData are of different type, it should not be equal
+            // Since the behavior was set to unchanged, that should be reflected in the output of toGDL
+            bool expected = true;
+            bool actual = target.ToGDL().Equals("Distance between points: unchanged 0%");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void toGDL_Range_test()
+        {
+            // The type we are testing
+            DistanceBetweenPoints target = new DistanceBetweenPoints()
+            {
+                Behaviour = BehaviourTypes.Range,
+                Max = 2,
+                Min = 0
+            };
+
+
+            // Since the behavior was set to range, that should be reflected in the output of toGDL
             bool expected = true;
             bool actual = target.ToGDL().Equals("Distance between points: 0..2");
 
-            Assert.AreEqual(expected, actual, "Behavior for type should be unchanged");
+            Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod()]
+        public void toGDL_Increasing_test()
+        {
+            // The type we are testing
+            DistanceBetweenPoints target = new DistanceBetweenPoints()
+            {
+                Behaviour = BehaviourTypes.Increasing,
+                Max = 2,
+                Min = 0
+            };
+
+
+            // Since the behavior was set to increasing, that should be reflected in the output of toGDL
+            bool expected = true;
+            bool actual = target.ToGDL().Equals("Distance between points: increasing");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void toGDL_Decreasing_test()
+        {
+            // The type we are testing
+            DistanceBetweenPoints target = new DistanceBetweenPoints()
+            {
+                Behaviour = BehaviourTypes.Decreasing,
+                Max = 2,
+                Min = 0
+            };
+
+
+            // Since the behavior was set to decreasing, that should be reflected in the output of toGDL
+            bool expected = true;
+            bool actual = target.ToGDL().Equals("Distance between points: decreasing");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        #endregion
     }
 }
