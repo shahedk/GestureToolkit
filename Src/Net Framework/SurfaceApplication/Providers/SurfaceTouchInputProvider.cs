cb98c51634566c0b23eb98a7d2e4a6d5a5a5ab01
Shahedul Huq Khandkar <shahedk@users.noreply.github.com>
2010-05-14 12:08:28 -0600

    1) Adding sample application project for Microsoft Surface and SMART Tabletop
    2) Removing "TestApplication" binaries from repository
    
    3) Updating VirtualTouchProvider to sync with the changes made in base class

---
+++ Src/Net Framework/SurfaceApplication/Providers/SurfaceTouchInputProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Framework.TouchInputProviders;
+
+namespace SurfaceApplication.Providers
+{
+    public class SurfaceTouchInputProvider : TouchInputProvider
+    {
+        public override event TouchInputProvider.FrameChangeEventHandler FrameChanged;
+
+        public override event TouchInputProvider.SingleTouchChangeEventHandler SingleTouchChanged;
+
+        public override event TouchInputProvider.MultiTouchChangeEventHandler MultiTouchChanged;
+
+        public override void Init()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
