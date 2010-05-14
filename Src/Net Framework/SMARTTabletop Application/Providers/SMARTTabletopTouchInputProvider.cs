7c3f14776febade35358a811e2d9132a8e669bf0
shahed <shahed@localhost>
2010-05-14 12:15:48 -0600

    Adding SMART Tabletop touch input provider
    Removing unnecessary binaries from repository

---
+++ Src/Net Framework/SMARTTabletop Application/Providers/SMARTTabletopTouchInputProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Framework.TouchInputProviders;
+
+namespace SMARTTabletop_Application.Providers
+{
+    public class SMARTTabletopTouchInputProvider : TouchInputProvider
+    {
+        public override event TouchInputProvider.FrameChangeEventHandler FrameChanged;
+
+        public override event TouchInputProvider.SingleTouchChangeEventHandler SingleTouchChanged;
+
+        public override event TouchInputProvider.MultiTouchChangeEventHandler MultiTouchChanged;
+    }
+}
