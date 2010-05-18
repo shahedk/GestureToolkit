734ecc6e7899db647e0682e70b8a05910fdbfb77
teddyseyed <teddyseyed@localhost>
2010-05-18 10:59:35 -0600

    Refactoring of work done yesterday

--- Src/Net Framework/SurfaceApplication/Providers/SurfaceTouchInputProvider.cs
+++ Src/Net Framework/SurfaceApplication/Providers/SurfaceTouchInputProvider.cs
@@ -9,6 +9,7 @@ using Microsoft.Surface;
 using Microsoft.Surface.Presentation.Controls;
 using Framework;
 using System.Windows;
+using Microsoft.Surface.Presentation;
 
 namespace SurfaceApplication.Providers
 {
@@ -38,64 +39,34 @@ namespace SurfaceApplication.Providers
             
         }
 
-        void _window_ContactLeave(object sender, Microsoft.Surface.Presentation.ContactEventArgs e)
+        public void _window_ContactLeave(object sender, ContactEventArgs e)
         {
-            //Get the point position from the ContactEventArgs, but the centered one, to be more accurate
-            Point position = e.Contact.GetCenterPosition(GestureFramework.LayoutRoot);
-
-            //Create a new touchinfo which will be used later to add a touchpoint
-            TouchInfo info = new TouchInfo();
-
-            //Set the action type of this to down, since the contact is down itself
-            info.ActionType = TouchAction2.Up;
-
-            //Set the position of the touchinfo to the previously found position from e
-            info.Position = position;
-
-            //Set the deviceid of the touchinfo to the id of the contact
-            info.TouchDeviceId = e.Contact.Id;
-
-            //add the new touch point to the base
-            base.UpdateActiveTouchPoint(info);
-
-            //work in progress, update the UpdateActiveTouchPoint 
-
+            //Call helper function to create appropriate touch point and register its call back
+            _touchPoint_and_CallBack_Creator(TouchAction2.Up, e);
         }
 
-        void _window_ContactChanged(object sender, Microsoft.Surface.Presentation.ContactEventArgs e)
+        public void _window_ContactChanged(object sender, ContactEventArgs e)
         {
-            //Get the point position from the ContactEventArgs, but the centered one, to be more accurate
-            Point position = e.Contact.GetCenterPosition(GestureFramework.LayoutRoot);
-
-            //Create a new touchinfo which will be used later to add a touchpoint
-            TouchInfo info = new TouchInfo();
-
-            //Set the action type of this to down, since the contact is down itself
-            info.ActionType = TouchAction2.Move;
-
-            //Set the position of the touchinfo to the previously found position from e
-            info.Position = position;
-
-            //Set the deviceid of the touchinfo to the id of the contact
-            info.TouchDeviceId = e.Contact.Id;
-
-            //add the new touch point to the base
-            base.UpdateActiveTouchPoint(info);
-
-            //work in progress, update the UpdateActiveTouchPoint 
-
+            //Call helper function to create appropriate touch point and register its call back
+            _touchPoint_and_CallBack_Creator(TouchAction2.Move, e);
         }
 
-        void _window_ContactDown(object sender, Microsoft.Surface.Presentation.ContactEventArgs e)
+        public void _window_ContactDown(object sender, ContactEventArgs e)
         {
-            //Get the  point position from the ContactEventArgs
+            //Call helper function to create appropriate touch point and register its call back
+            _touchPoint_and_CallBack_Creator(TouchAction2.Down, e);
+        }
+
+        public void _touchPoint_and_CallBack_Creator(TouchAction2 action, ContactEventArgs e)
+        {
+            //Get the  point position from the ContactEventArgs (can optionally use e.Contact.getCenterPosition here for more accuracy)
             Point position = e.GetPosition(GestureFramework.LayoutRoot);
 
             //Create a new touchinfo which will be used later to add a touchpoint
             TouchInfo info = new TouchInfo();
 
-            //Set the action type of this to down, since the contact is down itself
-            info.ActionType = TouchAction2.Down;
+            //Set the action type to the passed in action
+            info.ActionType = action;
 
             //Set the position of the touchinfo to the previously found position from e
             info.Position = position;
@@ -103,15 +74,25 @@ namespace SurfaceApplication.Providers
             //Set the deviceid of the touchinfo to the id of the contact
             info.TouchDeviceId = e.Contact.Id;
 
-            //add the new touch point to the base
-            var touchPoint = base.AddNewTouchPoint(info, e.OriginalSource as UIElement);
-
-            //Create the call back for the single touch point
-            if (SingleTouchChanged != null)
+            //If it is contact down, we want to add the point, otherwise we want to update that particular point
+            if (action == TouchAction2.Down)
             {
-                SingleTouchChanged(this, new SingleTouchEventArgs(touchPoint));
-            }
+                //add the new touch point to the base
+                var touchPoint = base.AddNewTouchPoint(info, e.OriginalSource as UIElement);
 
+                //Create the call back for the single touch point
+                if (SingleTouchChanged != null)
+                {
+                    SingleTouchChanged(this, new SingleTouchEventArgs(touchPoint));
+                }
+            }
+            else 
+            {
+                //add the new touch point to the base
+                base.UpdateActiveTouchPoint(info);
+
+                //work in progress, update the UpdateActiveTouchPoint 
+            }
         }
     }
 }
