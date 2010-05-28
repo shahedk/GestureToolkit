bf8efc967cfb2b1980329f9d0b002b70a4c1e50f
vanp <vanp@localhost>
2010-05-28 15:56:30 -0400

    Added Anoto Provider and application

---
+++ Src/AnotoApplication/AnotoApplication/Providers/AnotoInputProvider.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Framework.TouchInputProviders;
+using System.Windows.Controls;
+using Gestures.Objects;
+using System.Windows.Media;
+using mil.AnotoPen;
+using mil.conoto;
+
+namespace AnotoApplication.Providers
+{
+    public class AnotoInputProvider : TouchInputProvider
+    {
+        public override event FrameChangeEventHandler FrameChanged;
+
+        public override event MultiTouchChangeEventHandler MultiTouchChanged;
+
+        public override event SingleTouchChangeEventHandler SingleTouchChanged;
+
+        private AnotoStreamingServer anotoServer;
+        private static ConotoElementManager manager;
+        private static ConotoConvert conoto;
+        private Panel source;
+
+        public AnotoInputProvider(Panel LayoutRoot)
+        {
+            source = LayoutRoot;
+        }
+        public override void Init()
+        {
+            // Initialize configuration
+            manager = new ConotoElementManager();
+
+            // Load config file created by MultiPatternConfigurator (or any other conoto config tool)
+            // In this example the configuration file is stored in the folder "config".
+            // This file maps the A4 sized Anoto-Pattern-Page "70.0.0.1" to a 1024x768 sized screen area.
+            manager.LoadConfigFile("config/PatternConfig.pcf.xml");
+
+            // Initialize event converter (needs the element manager that holds the configuration)
+            conoto = new ConotoConvert(manager);
+
+            // Initialize AnotoPen with the license key
+            mil.AnotoPen.AnotoPen.Initialize("71ad1f27386ec4f5d91e15eca16f46c4");
+
+            // Initialize the real-time streaming receiver of AnotoPen
+            anotoServer = new AnotoStreamingServer();
+            // Add event handler to receive pen up/down/move events.
+            anotoServer.OnStroke += new AnotoStreamingServer.AnotoEventHandler(anotoServer_OnStroke);
+            // Start receiver
+            anotoServer.Start();
+        }
+
+        protected override void Dispose(bool value)
+        {
+            base.Dispose(value);
+
+            // Stop receiver and dispose AnotoPen assembly. 
+            // Otherwise the receiver thread will not be killed and the application process would
+            // be still running after exit.
+            anotoServer.Stop();
+            mil.AnotoPen.AnotoPen.Dispose();
+        }
+
+        private void anotoServer_OnStroke(object sender, AnotoPenEvent args)
+        {
+            // Initialize some variables onto the conoto converter will write to.
+            string function; // function string of the area hit by the pen (as configured in configuration)
+            long functionid; // function id of the area hit by the pen (as configured in configuration)
+            double x, y; // converted x/y coordinates
+            TouchInfo info = new TouchInfo();
+            TouchAction2 actionType = TouchAction2.Up;
+
+            // convert AnotoPen coordiantes to the configured ones (in this case, screen coordinates)
+            conoto.Convert((ulong)args.PageId, args.X, args.Y,
+                           out function, out functionid, out x, out y);
+            Point penPosition = new Point(x, y);
+            // handle the type of event we got and call other handler functions
+            switch (args.Type)
+            {
+                case AnotoPenEventType.StrokeStart: // = pen down
+                    actionType = TouchAction2.Down;
+                    break;
+                case AnotoPenEventType.StrokeDrag: // = pen move/drag (there is no hover due the AnotoPen does not support it)
+                    actionType = TouchAction2.Move;
+                    break;
+                case AnotoPenEventType.StrokeEnd: // = pen up
+                    actionType = TouchAction2.Up;
+                    break;
+            }
+            info.ActionType = actionType;
+            info.Position = penPosition;
+            info.TouchDeviceId = (int) args.PenId;
+            
+            //Use hit tests to find
+            UIElement element = null;
+            /*
+            for (int i = 0; i < source.Children.Count; i++)
+            {
+                IInputElement result = source.Children[i].InputHitTest(penPosition);
+                element = result as UIElement;
+            }*/
+ 
+            TouchPoint2 point = new TouchPoint2(info, source);
+            
+            if (SingleTouchChanged != null)
+            {
+                SingleTouchChanged(this, new SingleTouchEventArgs(point));
+            }
+        }
+    }
+}
