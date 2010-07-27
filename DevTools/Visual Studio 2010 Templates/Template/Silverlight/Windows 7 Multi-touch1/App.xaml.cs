4aea10f9f1c85c1b388715434d306dd9801d7457
shahed <shahed@localhost>
2010-07-26 22:31:15 -0600

    Added source files for Silverlight project template

---
+++ DevTools/Visual Studio 2010 Templates/Template/Silverlight/Windows 7 Multi-touch1/App.xaml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Windows_7_Multi_touch1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ DevTools/Visual Studio 2010 Templates/Template/Silverlight/Windows 7 Multi-touch1/Gesture Definitions/GestureDefinition1.g
@@ -0,0 +1,11 @@
+name: Drag2
+
+validation state
+    Touch state: TouchMove
+    Touch limit: 1
+
+condition
+    On same object
+
+return
+    Position, Position changed
